903dd93a005fb3fc4b89b161660b6137e09959cb
koinhug@126.com <koinhug@126.com>
2016-10-17 08:20:20 +0800

    纠错表构建

--- src/com/ztc/correct/TextProcessing.java
+++ src/com/ztc/correct/TextProcessing.java
@@ -6,9 +6,10 @@ import com.ztc.hanzi2pinyin.PinyinFormat;
 import com.ztc.hanzi2pinyin.PinyinHelper;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class TextProcessing {
     private static final String INPUT_CHINESE = "./res/input/correction/ref_word.txt";
@@ -19,6 +20,18 @@ public class TextProcessing {
     private static final String OUTPUT_TRUE_PINYIN = "./res/output/correction/true_pinyin.txt";
     private static final String OUTPUT_ERROR_PINYIN = "./res/output/correction/error_pinyin.txt";
 
+    private static final String ALL_OBSERVATIONS = "./res/output/all_observations.txt";
+
+    private static final String OUTPUT_CORRECTION_TABLE = "./res/output/correction/correction_table.txt";
+
+    private static final String[] notNeedCorrectionPinyins= new String[] {
+            "jv", "jvan", "jve", "jvn",
+            "qv", "qvan", "qve", "qvn",
+            "xv", "xvan", "xve", "xvn"
+    };
+
+    private static final ArrayList<String> allPinyins = new ArrayList<String>();
+    private static HashMap<String, ArrayList<String>> correctionMap = new HashMap<String, ArrayList<String>>();
 
     private static void initDir() {
         Utils.createDir(CORRECTION_OUTPUT_DIR);
@@ -82,6 +95,8 @@ public class TextProcessing {
                     builder.append(str);
                     builder.append(" ");
                 }
+                builder.deleteCharAt(builder.length() - 1);
+                builder.append("\n");
                 pinyinList.clear();
             }
             builder.deleteCharAt(builder.length() - 1);
@@ -118,6 +133,8 @@ public class TextProcessing {
                         builder.append(pinyin);
                         builder.append(" ");
                     }
+                    builder.deleteCharAt(builder.length() - 1);
+                    builder.append("\n");
                 } catch (PinyinException e) {
                     e.printStackTrace();
                 }
@@ -138,10 +155,73 @@ public class TextProcessing {
         }
     }
 
+    public static void generatorErrorStatistics() {
+        Utils.readFile(allPinyins, ALL_OBSERVATIONS);
+        BufferedReader buf1 = null, buf2 = null;
+        String line1 = null, line2 = null;
+        String[] line1Array = null, line2Array = null;
+        StringBuilder builder = new StringBuilder("");
+        try {
+            buf1 = new BufferedReader(new FileReader(OUTPUT_ERROR_PINYIN));
+            buf2 = new BufferedReader(new FileReader(OUTPUT_TRUE_PINYIN));
+            while ((line1 = buf1.readLine()) != null && (line2 = buf2.readLine()) != null) {
+                line1 = line1.trim();
+                line2 = line2.trim();
+                line1Array = line1.split(" ");
+                line2Array = line2.split(" ");
+                for (int i = 0; i < line1Array.length; i++) {
+                    if (!line1Array[i].equals(line2Array[i]) && !Arrays.asList(notNeedCorrectionPinyins).contains(line1Array[i])) {
+                        if (!correctionMap.containsKey(line1Array[i])) {
+                            correctionMap.put(line1Array[i], new ArrayList<String>());
+                        }
+                        if (!correctionMap.get(line1Array[i]).contains(line2Array[i])) {
+                            correctionMap.get(line1Array[i]).add(line2Array[i]);
+                        }
+                        if (allPinyins.contains(line1Array[i]) && !correctionMap.get(line1Array[i]).contains(line1Array[i])) {
+                            correctionMap.get(line1Array[i]).add(line1Array[i]);
+                        }
+                    }
+                }
+            }
+            for (Map.Entry<String, ArrayList<String>> entry : correctionMap.entrySet()) {
+                builder.append(entry.getKey());
+                builder.append("=");
+                for (String string : entry.getValue()) {
+                    builder.append(string);
+                    builder.append(" ");
+                }
+                builder.deleteCharAt(builder.length() - 1);
+                builder.append("\n");
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            Utils.writeStringToFile(new String(builder), OUTPUT_CORRECTION_TABLE);
+            buf1.close();
+            buf2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (buf1 != null) {
+                try {
+                    buf1.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (buf2 != null) {
+                try {
+                    buf2.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         initDir();
         generatorChinese();
         generatorErrorPinyin();
         generatorTruePinyin();
+        generatorErrorStatistics();
     }
 }
